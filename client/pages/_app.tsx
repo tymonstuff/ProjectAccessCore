467fc0d771c2f373ddd7ab17a67fcd7c3a3229c0
avhekkink <annavhekkink@gmail.com>
2022-08-10 11:14:18 +0100

    refactor: move web font to a custom Document and remove usage of  as recommended by warning messages

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 
 import * as React from 'react';
 import ReactDOM from 'react-dom';
@@ -7,33 +6,18 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import theme from '../theme/theme';
 import AppBar from '../components/AppBar/AppBar';
 
-import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react'
-import { frontendConfig } from '../config/frontendConfig'
+import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react';
+import { frontendConfig } from '../config/frontendConfig';
 
 if (typeof window !== 'undefined') {
   // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
-  SuperTokensReact.init(frontendConfig())
+  SuperTokensReact.init(frontendConfig());
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SuperTokensWrapper>
       <ThemeProvider theme={theme}>
-        <Head>
-          <title>Home App</title>
-          <link
-            rel="preconnect"
-            href="https://fonts.googleapis.com"
-          />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
         <AppBar />
         <Component {...pageProps} />
       </ThemeProvider>
