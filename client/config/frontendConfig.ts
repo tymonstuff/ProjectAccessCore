c98b5caa066bfe12252b91d0eaa50489ada9497f
avhekkink <annavhekkink@gmail.com>
2022-08-11 14:50:06 +0100

    feat: add example css styling to default supertokens UI

--- client/config/frontendConfig.ts
+++ client/config/frontendConfig.ts
@@ -1,13 +1,30 @@
-import EmailPasswordReact from 'supertokens-auth-react/recipe/emailpassword'
-import SessionReact from 'supertokens-auth-react/recipe/session'
-import { appInfo } from './appInfo'
+import EmailPasswordReact from 'supertokens-auth-react/recipe/emailpassword';
+import SessionReact from 'supertokens-auth-react/recipe/session';
+import { appInfo } from './appInfo';
 
 export const frontendConfig = () => {
   return {
     appInfo,
     recipeList: [
-      EmailPasswordReact.init(),
+      EmailPasswordReact.init({
+        style: {
+          container: {
+            fontFamily: `'Lato', sans-serif`,
+            marginTop: '10vh',
+          },
+          headerTitle: {
+            fontWeight: 'bolder',
+          },
+          link: {
+            color: '#3b8cd9',
+          },
+          button: {
+            backgroundColor: '#fda929',
+            border: '0px',
+          },
+        },
+      }),
       SessionReact.init(),
     ],
-  }
-}
+  };
+};
