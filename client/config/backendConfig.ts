ec652db8e4929bdf1f58d33abcf79d57cdc35851
avhekkink <annavhekkink@gmail.com>
2022-09-08 12:17:22 +0100

    feat: Add HTML email template for password recovery

--- client/config/backendConfig.ts
+++ client/config/backendConfig.ts
@@ -1,21 +1,24 @@
-import EmailPasswordNode from 'supertokens-node/recipe/emailpassword'
-import SessionNode from 'supertokens-node/recipe/session'
+import EmailPasswordNode from 'supertokens-node/recipe/emailpassword';
+import SessionNode from 'supertokens-node/recipe/session';
 import UserRoles from 'supertokens-node/recipe/userroles';
-import { appInfo } from './appInfo'
+import { appInfo } from './appInfo';
 import { TypeInput } from 'supertokens-node/types';
 
 import client from '@sendgrid/mail';
 
-import { getCurrentPayloadInfo } from '../lib/backendRoles'
+import * as fs from 'fs';
+
+import { getCurrentPayloadInfo } from '../lib/backendRoles';
 
 client.setApiKey(process.env.SENDGRID_API_KEY);
 
 export const backendConfig = (): TypeInput => {
   return {
-    framework: "express",
+    framework: 'express',
     supertokens: {
-      connectionURI: "https://0659919111a611ed8d48fd83c98df5cd-eu-west-1.aws.supertokens.io:3567",
-      apiKey: "N7LTW=f33BHQEyMawCOVE2Pm-Vb-36"
+      connectionURI:
+        'https://0659919111a611ed8d48fd83c98df5cd-eu-west-1.aws.supertokens.io:3567',
+      apiKey: 'N7LTW=f33BHQEyMawCOVE2Pm-Vb-36',
     },
     appInfo,
     recipeList: [
@@ -25,32 +28,36 @@ export const backendConfig = (): TypeInput => {
             return {
               ...originalImplementation,
               sendEmail: async function (input) {
-                if (input.type === "PASSWORD_RESET") {
+                if (input.type === 'PASSWORD_RESET') {
                   let { user, passwordResetLink } = input;
                   let { email, id, timeJoined } = user;
 
-                  console.log("EMAIL RESET FOR user: "+JSON.stringify(input))
+                  console.log('EMAIL RESET FOR user: ' + JSON.stringify(input));
+
+                  const emailHtml = fs
+                    .readFileSync('passwordRecoveryEmail.html')
+                    .toString();
 
                   const message = {
                     personalizations: [
                       {
                         to: [
                           {
-                            email: email
-                          }
-                        ]
-                      }
+                            email: email,
+                          },
+                        ],
+                      },
                     ],
                     from: {
                       email: '//////// TODO: get email',
-                      name: 'Project Access'
+                      name: 'Project Access',
                     },
                     subject: 'Password Recovery for Your Account',
                     content: [
                       {
                         type: 'text/html',
-                        value: '//////// TODO: get html'
-                      }
+                        value: emailHtml,
+                      },
                     ],
                     ipPoolName: 'transactional email',
                   };
@@ -59,9 +66,9 @@ export const backendConfig = (): TypeInput => {
                   //  .then(() => console.log('Password reset sent successfully'))
                   //  .catch(error => { console.error(error); });
                 }
-              }
-            }
-          }
+              },
+            };
+          },
         },
         override: {
           apis: (originalImplementation) => {
@@ -69,25 +76,25 @@ export const backendConfig = (): TypeInput => {
               ...originalImplementation,
               signUpPOST: async function (input) {
                 if (originalImplementation.signUpPOST === undefined) {
-                  throw Error("Should never come here");
+                  throw Error('Should never come here');
                 }
                 // Call the original implementation of signUpPOST.
                 let response = await originalImplementation.signUpPOST(input);
 
                 // Post sign up response, we check if it was successful
-                if (response.status === "OK") {
+                if (response.status === 'OK') {
                   let { id, email } = response.user;
-                  
+
                   // Input form fields values that the user used while signing up
                   let formFields = input.formFields;
 
                   //////// TODO: post sign up logic to send out google form
                 }
                 return response;
-              }
-            }
-          }
-        }
+              },
+            };
+          },
+        },
       }),
       SessionNode.init({
         override: {
@@ -96,11 +103,11 @@ export const backendConfig = (): TypeInput => {
               ...originalImplementation,
               createNewSession: async function (input) {
                 const userId = input.userId;
-                const customPayloadInfo = await getCurrentPayloadInfo(userId)
+                const customPayloadInfo = await getCurrentPayloadInfo(userId);
 
                 input.accessTokenPayload = {
                   ...input.accessTokenPayload,
-                  ...customPayloadInfo
+                  ...customPayloadInfo,
                 };
 
                 return originalImplementation.createNewSession(input);
@@ -109,9 +116,8 @@ export const backendConfig = (): TypeInput => {
           },
         },
       }),
-      UserRoles.init()
+      UserRoles.init(),
     ],
     isInServerlessEnv: true,
-  }
-}
-
+  };
+};
